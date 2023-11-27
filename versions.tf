9d0646d3bca792a826c160ca29c6d17c34216804
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-27 10:46:45 -0500

    v0.0.1

---
+++ versions.tf
@@ -0,0 +1,14 @@
+# Configure the minimum required providers supported
+
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.54.0"
+    }
+  }
+
+  required_version = ">= 1.3.1"
+
+}
