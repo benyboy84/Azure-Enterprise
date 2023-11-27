22224422e81977761dc15766c9e7c50714799af8
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-27 11:01:28 -0500

    v0.0.1

--- versions.tf
+++ versions.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.54.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=>2.23.0"
+    }
   }
 
   required_version = ">= 1.3.1"
